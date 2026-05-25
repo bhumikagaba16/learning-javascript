10b1c141098b5ef52bcb8a6a8656dc66bbc56801
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-05-25 14:29:35 +0530

    learning operations in js

--- folder1/conversionOperations.js
+++ folder1/conversionOperations.js
@@ -1,17 +1,36 @@
-let age = "33"
-age = "miku16"
-console.log(typeof age )
+// let age = "33"
+// age = "miku16"
+// console.log(typeof age )
 
-let valueInNumber = Number(age)
-console.log(typeof valueInNumber)
-console.log( valueInNumber)
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)
 
-let score = "abc"
-let booleanInScore = Boolean(score)
-console.log(booleanInScore)
-console.log(typeof booleanInScore)
+// let score = "abc"
+// let booleanInScore = Boolean(score)
+// console.log(booleanInScore)
+// console.log(typeof booleanInScore)
 
-let number = 21
-let stringNumber = String(number)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let number = 21
+// let stringNumber = String(number)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ============================ Operations ============================
+
+value = 5
+let negvalue = -value
+console.log(negvalue)
+
+str1 = "hello"
+str2 = " bhumi"
+str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log("1" + (2 + 2))
+console.log(1 + 2 + "2")
+console.log((1 + 2) + "2")
