558af68ba49fd30400ab280fe72049d970902b21
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-05-25 12:45:19 +0530

    learning conversion operations in js

---
+++ folder1/conversionOperations.js
@@ -0,0 +1,17 @@
+let age = "33"
+age = "miku16"
+console.log(typeof age )
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)
+
+let score = "abc"
+let booleanInScore = Boolean(score)
+console.log(booleanInScore)
+console.log(typeof booleanInScore)
+
+let number = 21
+let stringNumber = String(number)
+console.log(stringNumber)
+console.log(typeof stringNumber)
