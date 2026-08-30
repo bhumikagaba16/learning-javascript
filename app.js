9ddde2802be5c42872e8590029b7acd11156b0ef
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-08-30 12:43:16 +0530

    learning js

---
+++ app.js
@@ -0,0 +1,284 @@
+// console.log("hello");
+
+// let pencilPrice = 10;
+// let erasorPrice = 5;
+
+// let output = `The Price is ${pencilPrice + erasorPrice} rupees`;
+// console.log(output);
+
+
+// let age = 17;
+// if(age >= 18){
+//     console.log("you can drive");
+//     console.log("you can vote");
+// }
+// if(age < 18){
+//     console.log("you can not drive");
+//     console.log("you can not vote");
+    
+// }
+
+
+// let light = "yellow";
+
+// if(light === "red"){
+//     console.log("stop");    
+// }else if(light === "yellow"){
+//     console.log("ready to start");    
+// }else if(light === "green"){
+//     console.log("go");    
+// }
+
+// let marks = 50;
+// if(marks >= 100){
+//     console.log("A++"); 
+// }else if(marks >= 80){
+//     console.log("A");
+// }else if(marks >= 60){
+//     console.log("B++");
+// }else if(marks >= 40){
+//     console.log("B");
+// }else if(marks >= 35){
+//     console.log("C");
+// }else{
+//     console.log("fail");  
+// }
+
+// let month = "may";
+// if(month === "january"){
+//     console.log("winter season");
+// }else if(month === "may"){
+//     console.log("summer season");
+    
+// }
+
+
+
+// let size = "f";
+
+// if(size === "XL"){
+//     console.log("Price of Popcorn is Rs.250");
+// }else if(size === "L"){
+//     console.log("Price of Popcorn is Rs. 200");
+// }else if(size === "M"){
+//     console.log("Price of Popcorn is Rs. 100");
+// }else if(size === "S"){
+//     console.log("Price of Popcorn is Rs. 50");   
+// }else{
+//     console.log("size is not available");
+    
+// }
+
+// let marks = 95;
+
+// if(marks>=33){
+//     if(marks>=80){
+//         console.log("O");
+//     }else{
+//         console.log("A");
+//     }
+// }else{
+//     console.log("Better Luck next time"); 
+// }
+
+// let marks = 64;
+// if(marks>=33 && marks>=80){
+//     console.log("pass");
+//     console.log("A");  
+// }else{
+//     console.log("fail");
+    
+// }
+
+// let str = "apple";
+
+// if(str[0] === 'a' && str.length>3){
+//     console.log("Good String");
+// }else{
+//     console.log("Not a Good String");
+// }
+
+// let num = 12;
+// if((num%3 === 0) && ((num+1 == 15) || (num-1 == 11))){
+//     console.log("safe");
+// }else{
+//     console.log("unsafe");
+// }
+
+// let color = "green";
+// switch(color){
+//     case "red":
+//         console.log("stop");
+//         break;
+//     case "yellow":
+//         console.log("slow down");
+//         break;
+//     case "green":
+//         console.log("go");
+//         break;
+//     default:
+//         console.log("broken light");
+// }
+
+// let day = 5;
+
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("wrong day");
+        
+// }
+
+// alert("hello people");
+// console.error("this is an error message");
+// console.warn("This is a warning message");
+
+// let name = prompt("please enter your name :");
+// console.log(name);
+
+// let firstName = prompt("Enter your First Name:");
+// let lastName = prompt("Enter your Last Name:");
+// let msg = "Welcome "+firstName +" "+lastName;
+// alert(msg);
+
+
+
+// let num = 20;
+// if((num/10) && (num%10 === 0)){
+//     console.log("good");
+// }else{
+//     console.log("bad");
+    
+// }
+
+// let userName = prompt("Enter your name:");
+// let userAge = prompt("Enter your age:");
+// let msg = `${userName} is ${userAge} years old.`;
+// alert(msg);
+
+// let quarter = 3;
+// switch (quarter) {
+//     case 1:
+//         console.log("January, Febraury, March");
+//         break;
+//     case 2:
+//         console.log("April, May, June");
+//         break;
+//     case 3:
+//         console.log("July, August, September");
+//         break;
+//     case 4:
+//         console.log("October, November, Dcember");
+//         break;
+
+//     default:
+//         console.log("no quarter found");
+//         break;
+// }
+
+// let str = "amrit";
+
+// if ((str[0] == 'A' || str[0] == 'a') && (str.length > 5)){
+//     console.log("golden string");
+// }else{
+//     console.log("not a golden string");
+// }
+
+// let a = 4;
+// let b = 5;
+// let c = 22;
+
+// if(a>b && a>c){
+//     console.log(a + " is the greatest number");
+// }else if(b>c){
+//     console.log(b + " is the greatest number");
+// }else{
+//     console.log(c + " is the greatest number");
+// }
+
+// let num1 = 56;
+// let num2 = 876;
+
+// if((num1%10) == (num2%10)){
+//     console.log("both numbers have same last digit ",num1%10);
+// }else{
+//     console.log("both numbers have different last digits");   
+// }
+
+// let msg = "        hello      ";
+// console.log(msg.trim());
+
+// let pass = prompt("Enter your password:");
+// console.log(pass.trim())
+
+// let msg = "Bhumika";
+// console.log(msg.toUpperCase());
+// console.log(msg.toLowerCase());
+
+// let str = "ILoveCoding";
+// console.log(str.indexOf("love"));
+// console.log(str.indexOf("Love"));
+// console.log(str.indexOf("o"));
+
+// let str = "          bhumi  ";
+// console.log(str.trim().toUpperCase());
+
+// let str = "HelloBeautifull";
+// console.log(str.slice(1,5));
+// console.log(str.slice(5,15));
+// console.log(str.slice(-1));
+// console.log(str.replace("Beautifull","world"));
+// console.log(str.repeat(3));
+
+// let msg = "help!";
+// console.log(msg.trim().toUpperCase());
+
+// let str = "apnaCollege";
+// console.log(str.slice(4).replace("l","t"));
+
+// let start = ["january","july","march","aungust"];
+// (start.shift());
+// (start.shift());
+// (start.unshift("june"));
+// (start.unshift("july"));
+// console.log(start);
+
+// let arr =["apple", "mango", "banana"];
+// console.log(arr.indexOf("mango"));
+// console.log(arr.includes("lichi"));
+// console.log(arr.includes("apple"));
+
+
+let colors = ["red", "yellow", "blue", "pink", "white", "green"];
+
+// console.log(colors.slice());
+// console.log(colors.slice(1));
+// console.log(colors.slice(-2));
+
+// let start =["january", "july", "march", "august"];
+// console.log(start.splice(0,1));
+// console.log(start.splice(1,0,"june"));
+// console.log(start);
+
+let lang = ["c", "c++", "html", "javascript", "python", "java", "c#", "sql"];
+console.log(lang.reverse());
