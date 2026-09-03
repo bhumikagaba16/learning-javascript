d88c7f18e6559c4a498e864cc65da03baafefda1
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-09-03 16:41:02 +0530

    learning js

---
+++ app.js
@@ -0,0 +1,58 @@
+//================= FOR LOOPS =================
+
+// for(let i = 1; i<=10; i++){
+//     console.log(i);
+// }
+
+//------------- PRINT ALL ODD NUMBERS(1-15) -------------
+
+// for(let i = 1; i<=15; i=i+2){
+//     console.log(i);
+// }
+
+// for(let i=15; i>=1; i=i-2){
+//     console.log(i);
+// }
+
+//-------------- PRINT ALL EVEN NUMBERS(2-10) --------------
+
+// for(let i=2; i<=10; i=i+2){
+//     console.log(i);
+// }
+
+//--------------- PRINT THE MULTIPLICATION TABLE FOR 5 ---------------
+
+// for(let i=5; i<=50; i=i+5){
+//     console.log(i);
+// }
+
+//=============== NESTED FOR LOOPS ===============
+
+// for(let i=1; i<=3; i++){
+//     console.log(`outer loop ${i}`);
+//     for(let j=1; j<=3; j++){
+//         console.log(j);
+//     }
+// }
+
+//============== WHILE LOOPS ===============
+
+// let i = 1;
+// while(i<=5){
+//     console.log(i);
+//     i++
+// }
+
+//------- EVEN-ODD NUMBER (WHILE LOOPS) ---------------
+
+// let i = 1;
+// while(i<=10){
+//     console.log(i);
+//     i=i+2;
+// }
+
+let i = 2;
+while(i<=10){
+    console.log(i);
+    i=i+2;
+}
