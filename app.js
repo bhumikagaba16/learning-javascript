b76434e2e66e8f3b316cdb46cc22d378bc7bddd1
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-09-06 18:30:52 +0530

    learning js

---
+++ app.js
@@ -0,0 +1,64 @@
+// =====object literals========
+
+// let student = {
+//     name: "bhumi",
+//     age: 20,
+//     marks: 85,
+// };
+
+// =========post===========
+// let post = {
+//     username: "@bhumikagaba",
+//     content: "hello everyone",
+//     likes: 100,
+//     repost: 20,
+//     tags: "viral",
+// };
+
+// =============GUESSING GAME===========
+
+// let value = prompt("Enter the range for your Random number");
+// let randomNum = Math.floor(Math.random()*value)+1;
+// let guess = prompt(`guess the number between 1 to ${value}`);
+// while(true){
+//     if(guess == "quit"){
+//         console.log("quitting the game");
+//         break;
+//     }
+//     if(guess == randomNum){
+//         console.log(`congratulation your guessing number ${guess} is correct!!!`);
+//         break;
+//     }else {
+//         console.log("wrong guess");
+//         if(guess<randomNum){
+//             console.log(`the number is greater than ${guess}`);
+//         }
+//         else{
+//             console.log(`the number is less than ${guess}`)
+//         }
+//          guess = prompt(`please guess the number again between 1 to ${value}`);
+//     }
+// }
+// Create a program that generates a random number representing a dice roll. 
+// [The number should be between 1 and 6].
+
+// let number = Math.floor(Math.random()*6)+1;
+// console.log(`dice roll... number comes ${number}`);
+
+
+// let car = {
+//     name:"fotuner",
+//     model:"legender",
+//     color:"black"
+// };
+// console.log(car.name);
+
+
+// let person={
+//     name:"bhumika",
+//     age:"20",
+//     city:"panipat"
+// };
+// person.city = "New York";
+// person.country = "united states";
+// console.log(person);
