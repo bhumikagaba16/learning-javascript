7ea07cf22a17573e2312ea741456d5f9d19b89e7
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-09-07 22:14:06 +0530

    learning js

---
+++ app.js
@@ -0,0 +1,79 @@
+// function hello(){
+//     console.log("hello everyone");
+    
+// }
+
+
+// function printName(){
+//     console.log("bhumika");
+    
+// }
+
+// function vote(){
+//     let age = 20;
+//     if(age>18){
+//         console.log("Adult, can vote");
+//     }else{
+//         console.log("can't vote");
+//     }
+// }
+
+
+// function printPoem(){
+//     console.log("Twinkle Twinkle, little star");
+//     console.log("How I Wonder What You Are");
+// }
+
+
+// function dice(){
+//     let random = Math.floor(Math.random()*6)+1;
+//     console.log(random);
+// }
+
+
+// function PrintName(name){
+//     console.log(name);
+    
+// }
+
+// function printInfo(name, age){
+//     // console.log(name);
+//     // console.log(age);
+//     console.log(`${name}'s age is ${age}`);
+    
+// }
+// printInfo("bhumika", 20)
+
+// function avg(a, b, c){
+//     let sum = (a + b + c)/3;
+//     console.log(sum);
+    
+// }
+// avg(5,2,2);
+
+// function table(num){
+//     for(let i = num; i <= 10*num; i = i+num){
+//         console.log(i);
+//     }
+// }
+// table(5);
+
+// function sum(a, b){
+//     return a+b;
+// }
+
+// function getSum(n){
+//     let sum = 0;
+//     for(let i = 0; i <= n; i++){
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+
+
+let sum = 50; // GLOBAL SCOPE
+function calcSum(a, b){
+    let sum = a + b; // function scope
+    console.log(sum);
+}
+// console.log(sum);
