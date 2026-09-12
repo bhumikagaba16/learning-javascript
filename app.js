4173b97d5fc1cb299edc5f2b3e435cb4d5baff0a
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-09-12 13:35:24 +0530

    leaning js

--- app.js
+++ app.js
@@ -77,3 +77,100 @@
 //     console.log(sum);
 // }
 // // console.log(sum);
+
+// function multipleGreet(func,n){
+//     for(let i=1; i<=n; i++){
+//         func();
+        
+//     }
+// }
+
+// let greet = function(){
+//     console.log("Good Morning");
+// }
+
+// multipleGreet(greet,5);
+
+// function oddEvenTest(request){
+//     if(request == "odd"){
+//         let odd = function(n){
+//             console.log(!(n%2 == 0));
+//         }
+//         return odd;
+//     }else if(request == "even"){
+//         let even = function(n){
+//             console.log(n%2 == 0);
+//         }
+//         return even;
+//     }else{
+//         console.log("wrong request");   
+//     }
+// }
+// let request = "odd";
+// let func = oddEvenTest(request);
+
+//===============
+
+// let arr = [19,10,4,7,20];
+// let num = 12;
+// function bigArr(arr, num){
+//     for(let i=0; i<arr.length; i++){
+//         if(arr[i]>num){
+//             console.log(arr[i]);
+//         }
+//     }
+// }
+// bigArr(arr,num);
+
+// let str ="abcdabcdefgggh";
+// function getStr(str){
+//     let ans = "";
+//     for(let i=0; i<str.length; i++){
+//         let currentStr = str[i];
+//         if(ans.indexOf(currentStr) == -1){
+//             ans+=currentStr;
+//         }
+//     }
+//     return ans;
+// }
+// let newStr = getStr(str);
+// console.log(newStr);
+
+// let arr = ["Australia","United States of America","Germany"];
+// function country(arr){
+//     let ans = "";
+//     for(let i=0; i<arr.length; i++){
+//         let currCountry = arr[i];
+//         if(currCountry.length > ans.length){
+//             ans =currCountry;
+//         }
+//     }
+//     return ans;
+// }
+// let newCountry =country(arr);
+// console.log(newCountry);
+
+// let str = "bhumikaaa";
+//  function vowelStr(str){
+//     let vowel = "aeiou";
+//     let count = 0;
+//     for(let i=0; i<str.length; i++){
+//         let currStr = str[i];
+//         if(vowel.indexOf(currStr) != -1){
+//             count+=1;
+//         }
+//     }
+//     return count;
+//  }
+// let newStr = vowelStr(str);
+// console.log(newStr);
+
+
+let startNum = 11;
+let endNum = 20;
+function randomNum(startNum, endNum){
+    let diff = endNum-startNum;
+    let num = Math.floor(Math.random()*diff)+startNum;
+    console.log(num);
+}
+randomNum(startNum, endNum);
